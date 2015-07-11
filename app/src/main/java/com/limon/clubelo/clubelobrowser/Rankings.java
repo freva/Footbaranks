a29dbfd6dadd0038f276347d316eae51ea409f17
freva <valerijf@stud.ntnu.no>
2015-07-11 16:08:51 +0200

    Added listener to toolbar spinner

--- app/src/main/java/com/limon/clubelo/clubelobrowser/Rankings.java
+++ app/src/main/java/com/limon/clubelo/clubelobrowser/Rankings.java
@@ -5,28 +5,35 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ArrayAdapter;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Spinner;
 
 import com.limon.clubelo.clubelobrowser.adapters.TeamRankingAdapter;
 import com.limon.clubelo.clubelobrowser.adapters.ToolbarDateRankingsAdapter;
+import com.limon.clubelo.clubelobrowser.containers.ToolbarDateRankingsItem;
 import com.limon.clubelo.clubelobrowser.tasks.TeamRankingsTask;
 import com.limon.clubelo.clubelobrowser.tasks.interfaces.TeamRankingsCallback;
 import com.limon.clubelo.clubelobrowser.tasks.responce.TeamRanking;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 
-public class Rankings extends AppCompatActivity implements TeamRankingsCallback {
-    private Toolbar toolbar;
+public class Rankings extends AppCompatActivity implements TeamRankingsCallback, AdapterView.OnItemSelectedListener {
+    private static final DateFormat df = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
+    private List<TeamRanking> teamRankings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rankings);
 
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
         //toolbar.setNavigationIcon(R.drawable.);
@@ -36,8 +43,12 @@ public class Rankings extends AppCompatActivity implements TeamRankingsCallback 
 
         ToolbarDateRankingsAdapter adapter = new ToolbarDateRankingsAdapter(this.getApplicationContext());
         mNavigationSpinner.setAdapter(adapter);
+        mNavigationSpinner.setOnItemSelectedListener(this);
 
-        new TeamRankingsTask(this, this).execute("2015-01-01");
+        if(teamRankings == null) {
+            Date today = new Date();
+            new TeamRankingsTask(this, this).execute(df.format(today));
+        }
     }
 
     @Override
@@ -64,9 +75,21 @@ public class Rankings extends AppCompatActivity implements TeamRankingsCallback 
 
     @Override
     public void onTeamRankingsReceived(List<TeamRanking> teamRankings) {
+        this.teamRankings = teamRankings;
         TeamRankingAdapter adapter = new TeamRankingAdapter(this.getApplicationContext(), teamRankings);
 
         ListView listView = (ListView) findViewById(R.id.lvTeams);
         listView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        Date getDate = ((ToolbarDateRankingsItem) parent.getItemAtPosition(position)).getDate();
+        new TeamRankingsTask(this, this).execute(df.format(getDate));
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
--- app/src/main/java/com/limon/clubelo/clubelobrowser/containers/ToolbarDateRankingsItem.java
+++ app/src/main/java/com/limon/clubelo/clubelobrowser/containers/ToolbarDateRankingsItem.java
@@ -3,14 +3,17 @@ package com.limon.clubelo.clubelobrowser.containers;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
 public class ToolbarDateRankingsItem {
-    private static final DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+    private static final DateFormat df = new SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH);
     private String title, value;
+    private Date date;
 
     public ToolbarDateRankingsItem(String title, Date date) {
         this.title = title;
         this.value = df.format(date);
+        this.date = date;
     }
 
     public ToolbarDateRankingsItem(String title) {
@@ -24,5 +27,9 @@ public class ToolbarDateRankingsItem {
 
     public String getValue() {
         return value;
+    }
+
+    public Date getDate() {
+        return date;
     }
 }
